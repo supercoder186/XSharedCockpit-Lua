de29b5c500325774edc06f40c6c45a6c11039a0b
Aadit Kolar <aadit.kolar@gmail.com>
2020-05-08 14:53:10 +0530

    Commented both lua scripts (you're welcome)

--- XSharedCockpit.lua
+++ XSharedCockpit.lua
@@ -1,5 +1,8 @@
-ini = require 'Resources.plugins.FlyWithLua.Scripts.XSharedCockpit.ini'
+--require the ini parser file (ini.lua) and the default lua sockets library
+local ini = require 'Resources.plugins.FlyWithLua.Scripts.XSharedCockpit.ini'
+local socket = require 'socket'
 
+--define the datarefs arrays and add the basic flight path datarefs
 local drefs = {}
 drefs[1] = "sim/flightmodel/position/local_x"
 drefs[2] = "sim/flightmodel/position/local_y"
@@ -14,25 +17,22 @@ drefs[10] = "sim/flightmodel/position/local_vx"
 drefs[11] = "sim/flightmodel/position/local_vy"
 drefs[12] = "sim/flightmodel/position/local_vz"
 
-
 slow_drefs = {}
 
+--define all the basic variables required
 local master_address = '127.0.0.1'
-local slave_address = '127.0.0.1'
-local slave_port = 49001
 local master_port = 49000
 local running = false
 local is_server = true
 local is_connected = false
-local socket = require "socket"
 local config_file_path = AIRCRAFT_PATH .. 'smartcopilot.cfg'
-local written = false
 
 local config = ini.parse_file(config_file_path)
 
 local master_overrides = {}
 local slave_overrides = {}
 
+--pretty self-explanatory
 function write_to_file(text, filename)
     local file = io.open(filename, 'r')
     io.output(file)
@@ -42,19 +42,20 @@ function write_to_file(text, filename)
     print("file written")
 end
 
+--split a string by the separator
 function split (inputstr, sep)
-        if sep == nil then
-                sep = "%s"
-        end
-        local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-            t[#t+1] = str
-        end
-        return t
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[#t+1] = str
+    end
+    return t
 end
 
+--(Checks if number has check as one of its addition factors)
 local function number_contains(number, check)
-    --(Checks if number has check as one of its addition factors)
     i = 16
     while i >= 1 do
         if number > i then
@@ -67,6 +68,8 @@ local function number_contains(number, check)
     end
     return false
 end
+
+--Add all the required datarefs to drefs and slow_drefs
 
 local triggers = config["TRIGGERS"]
 if triggers then
@@ -136,50 +139,89 @@ if radios then
     end
 end
 
+--check if a string is empty
 local function isempty(s)
   return s == nil or s == ''
 end
 
 function start_server()
+    --tell the program that the server is running
     running = true
-    master = socket.udp()
-    master:setsockname(master_address, master_port)
-    master:setpeername(slave_address, slave_port)
     print("Starting master broadcaster")
+    --bind the server to the address and port
+    server = assert(socket.bind(master_address, master_port))
+    --accept a client connection
+    master = server:accept()
+    print("Slave connection accepted")
+    --tell the program that the server is connected to a client and that broadcasts can be made
     is_connected = true
 end
 
 function stop_server()
     print("Stopping master broadcaster")
+    --tell the slave that the server is turning off
     broadcast_datarefs("close")
+    --tell the program that the server is no longer connected to a client
+    is_connected = false
+    --tell the program that the server is no longer running
     running = false
     master:close()
 end
 
 function start_slave()
+    --tell the program that the slave is running
     running = true
-    for k, v in pairs(slave_overrides) do
-        set(v, 1)
+
+    --set all the slave overrides
+    for k,v in ipairs(slave_overrides) do
+        --parse the dataref name and possibly index
+        dfname = ini.parse_dfname(v)
+        if #dfname == 2 then --it is an array dataref
+            set_array(dfname[1], dfname[2], 1)
+        else if #dfname == 1 then --it is a float dataref
+            set(dfname[1], 1)
+        end
     end
+
+    --override the xplane flight model
     set_array("sim/operation/override/override_planepath", 0, 1)
-    slave = socket.udp()
+
+    --initialise the slave
+    slave = assert(socket.tcp())
+
+    --connect to the server
+    slave:connect(master_address, master_port)
     print("Starting receiver")
-    slave:setsockname(slave_address, slave_port)
-    slave:setpeername(master_address, master_port)
     slave:settimeout(0)
+
+    --Notify the program that the slave is connected to the server
     is_connected = true
 end
 
 function stop_slave()
+    --Notify the program that the slave is no longer connected
+    is_connected = false
+
+    --Notify the program that the slave has stopped running
     running = false
-    for k, v in pairs(slave_overrides) do
-        set(v, 0)
+
+    --Disable all the slave overrides
+    for k,v in ipairs(slave_overrides) do
+        dfname = ini.parse_dfname(v)
+        if #dfname == 2 then
+            set_array(dfname[1], dfname[2], 0)
+        end
     end
+
+    --Re-enable the flight model
     set_array("sim/operation/override/override_planepath", 0, 0)
     print("Stopping receiver")
+
+    --Close the slave connection
     slave:close()
 end
 
+--Pretty much self-explanatory
 function toggle_master()
     if ((not(is_master)) and (running)) then
         print("Invalid! Slave is already running")
@@ -191,6 +233,7 @@ function toggle_master()
     end
 end
 
+--Pretty much self-explanatory
 function toggle_slave()
     if is_master and running then
         print("Invalid! Master is already running")
@@ -202,64 +245,84 @@ function toggle_slave()
     end
 end
 
+--Broadcast the data inside the input parameter
 function broadcast_datarefs(data)
     master:send(data)
 end
 
 function send_datarefs()
+    --Check if the server is connected to a slave
     if not is_connected then
         return
     end
     dataref_string = ""
     for k, v in ipairs(drefs) do
+        --Parse the dataref name and possibly the index
         parsed = ini.parse_dfname(v)
-        if #parsed == 2 then
-            value = get(parsed[1], parsed[2])
+        if #parsed == 2 then --It is an array dataref
+            value = get(parsed[1], parsed[2]) --Get the dataref value
             if value then
                 dataref_string = dataref_string .. k .. "=" .. value .. " "
             end
-        elseif #parsed == 1 then
+        elseif #parsed == 1 then --It is a float dataref
+            value = get(parsed[1]) --Get the dataref value
             if value then
                 dataref_string = dataref_string .. k .. "=" .. value .. " "
             end
         end
     end
+    --Append new line so that the receiver knows that the dataref transmission for this cycle is complete
+    dataref_string = dataref_string..'\n'
+
+    --Broadcast dataref string
     broadcast_datarefs(dataref_string)
 end
 
 function send_slow_datarefs()
+    --Check if the server is connected to a slave
     if not is_connected then
         return
     end
     dataref_string = ""
     for k, v in ipairs(slow_drefs) do
+        --Parse the dataref name and possibly the index
         parsed = ini.parse_dfname(v)
-        if #parsed == 2 then
-            value = get(parsed[1], parsed[2])
+        if #parsed == 2 then --It is an array dataref
+            value = get(parsed[1], parsed[2]) --Get the dataref value
             if value then
-                dataref_string = dataref_string .. v .. "=" .. value .. " "
+                dataref_string = dataref_string .. k .. "=" .. value .. " "
             end
-        elseif #parsed == 1 then
-            value = get(parsed[1])
+        elseif #parsed == 1 then --It is a float dataref
+            value = get(parsed[1]) --Get the dataref value
             if value then
-                dataref_string = dataref_string .. v .. "=" .. value .. " "
+                dataref_string = dataref_string .. k .. "=" .. value .. " "
             end
         end
     end
+    --Append new line so that the receiver knows that the dataref transmission for this cycle is complete
+    dataref_string = dataref_string..'\n'
+
+    --Broadcast dataref string
     broadcast_datarefs(dataref_string)
 end
 
 local function set_datarefs(s)
-    s = split(s, " ")
+    s = split(s, " ") --make an array of all the datarefs
     for k, v in ipairs(s) do
+        -- Parse the index number
         idx = v:match('(%S+)%s*=')
+        --Convert it to a number
         idx_n = tonumber(idx)
         if idx then
+            --Replace the index number with the dataref name
             v = v:gsub(idx, drefs[idx_n], 1)
+
+            --Parse the dataref value
             data = ini.parse_data(v)
-            if #data[2] == 2 then
+
+            if #data[2] == 2 then --it is an array dataref
                 set_array(data[1], data[2][1], data[2][2])
-            elseif #data[2] == 1 then
+            elseif #data[2] == 1 then --it is a float dataref
                 set(data[1], data[2][1])
             end
         end
@@ -267,43 +330,59 @@ local function set_datarefs(s)
 end
 
 function sync_datarefs()
-    if(not(is_connected)) then
-        return
-    end
-    received = slave:receive()
-    if(isempty(received)) then
+    --Check if the slave is connected
+    if not is_connected then
         return
     end
 
+    --Receive the server's transmissions
+    received, error = slave:receive('*l')
+
+    --Check if received data is valid
+    if isempty(received) or error == 'closed' then
+        return
+    end
+
+    --Check if the server is calling for a close
+    if received == 'close' then
+        toggle_slave()
+        return
+    end
+
+    --Set the datarefs according to the received message
     set_datarefs(received)
 end
 
+--register X-Plane commands to toggle master and slave
 create_command("XSharedCockpit/toggle_master", "Toggle XSharedCockpit as Master","toggle_master()", "", "")
 create_command("XSharedCockpit/toggle_slave", "Toggle XSharedCockpit as Slave", "toggle_slave()", "", "")
+
+--register FlyWithLua macros to toggle master and slave
 add_macro("Toggle XSharedCockpit as Master", "toggle_master()")
 add_macro("Toggle XSharedCockpit as Slave", "toggle_slave()")
 
 count = 0
 
 function loop()
+    --check if something is running
     if running then
-        if is_master then
-            send_datarefs()
-        else
-            sync_datarefs()
+        if is_master then --master is running
+            send_datarefs() --broadcast datarefs
+        else --slave is running
+            sync_datarefs() --sync the datarefs with master
         end
         if count % 10 == 0 then
-            often()
+            --often()
         end
         count += 1
     end
 end
 
 function often()
-    if running and is_master then
-        send_slow_datarefs()
+    if running and is_master then --master is running
+        send_slow_datarefs() --send the datarefs under SLOW and WEATHER
     end
 end
 
 --do_often("often()")
-do_every_draw("loop()")
+do_every_draw("loop()") --tell FlyWithLua to run loop() every time X-Plane draws
--- ini.lua
+++ ini.lua
@@ -1,56 +1,77 @@
 local ini = {}
 
+--Check if a string is not empty
 local function isnotempty(s)
   return not(s == nil or s == '')
 end
 
+--Removes all ini-style comments from a string
 local function remove_comments(s)
+    --Define regex string
     rgx = '[ ]*[#;]+.*'
+    --Match all commented sections
     match = s:match(rgx)
-    if isnotempty(match) then
+    if isnotempty(match) then --There are comments in the string
+        --Remove the commented section
         s = s:gsub(match, "")
     end
     return s
 end
 
+--Used to detect if a line is a section header
 local function detect_section(s)
+    --Define regex string (trust me it's not that complicated)
     rgx = '%[([^%[%]]+)%]$'
+    --Match the string to the regex, to see if it is a section header
     match = s:match(rgx)
-    if isnotempty(match) then
+    if isnotempty(match) then --It is a section header
         return match
     end
 end
 
+--Used to parse a data line in the ini file
 function parse_data(s)
+    --Define regex string
     rgx = '(%S+)[ ]*=[ ]*(%S+)'
     table = {}
+    --Iterate over each match
     for k, v in string.gmatch(s, rgx) do
+        --Add the values to the table
         table[1] = k
         table[2] = tonumber(v)
     end
     return table
 end
 
+--Parses the dataref name and possibly index
 function ini.parse_dfname(s)
+    --Define regex string
     rgx = '(%S+)%[(%d+)%]'
+
+    --Remove some smartcopilot annotations
     s = s:gsub("_FIXED_INDEX_", "")
+
+    --Iterate over all match groups if it matches
+    --If s:gmatch() gives a non-nil value, this means that it is an array dataref
     for k, v in s:gmatch(rgx) do
-        return {k, tonumber(v)}
+        return {k, tonumber(v)} --Return parsed dataref name and array index
     end
-    return {s}
+    return {s} --The dataref name is not an array dataref, return it
 end
 
+--Parses the dataref name, optionally dataref index, and dataref value
 function ini.parse_data(s)
+    --Define regex strings
     rgx = '(%S+)[ ]*=[ ]*(%S+)'
     array_rgx = '(%S+)[%s]*%[(%d+)%][ ]*=[ ]*(%S+)'
     table = {}
-    if s:match('%[%d+%]') then
-        for k, i, v in string.gmatch(s, array_rgx) do
+    if s:match('%[%d+%]') then --It contains square brackets, indicating that it is an array dataref
+        for k, i, v in string.gmatch(s, array_rgx) do --Get the name, index and value
             table[1] = k
             table[2] = {tonumber(i), tonumber(v)}
         end
     else
-        for k, v in string.gmatch(s, rgx) do
+        for k, v in string.gmatch(s, rgx) do --Get the name and value
             table[1] = k
             table[2] = {tonumber(v)}
         end
@@ -58,26 +79,36 @@ function ini.parse_data(s)
     return table
 end
 
+--Parses the smartcopilot.cfg file to give the ini file in a table structure
 function ini.parse_file(file_name)
+    --Open the file
     file = io.open(file_name, "r")
     io.input(file)
+
+    --Setup the basic variables
     current_section = ""
     t = {}
 
-    for line in io.lines() do
+    for line in io.lines() do --Iterate over each line of the file
+        --First remove the comments from the line
         line = remove_comments(line)
+
+        --See if the line contains a section header
         section_name = detect_section(line)
-        if section_name then
-            current_section = section_name
-            t[current_section] = {}
+        if section_name then --Line is a section header
+            current_section = section_name --Update the current section
+            t[current_section] = {} --Initialise the array for this section
         else
-            data = parse_data(line)
+            data = parse_data(line) --Parse the dataref name
             if data and #data == 2 then
                 t[current_section][data[1]] = data[2]
             end
         end
     end
+
+    --Return the table representing the parsed file
     return t
 end
 
+--Return the ini object to be used by other programs
 return ini
